1dcb5bffaf085031a79f2415e28381d93c37e8f3
namhibi <115703000+namhibi@users.noreply.github.com>
2023-11-15 14:32:52 +0700

    Delete bai1_4.js

--- bai1_4.js
+++
@@ -1,6 +0,0 @@
-function getExtensionOfFile(str) {
-    let indexOfDot = str.indexOf(".");
-    return str.slice(indexOfDot + 1);
-}
-console.log(getExtensionOfFile("image.png"));
-console.log(getExtensionOfFile("music.mp3"));
